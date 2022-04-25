41e4186301174e5330b4d3f0e88f84aa0b338740
Stijndus <stijn@ardusit.nl>
2022-04-25 16:32:58 +0200

    Added html en ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthStateService } from './shared/auth-state/auth-state.service';
+import { TokenService } from './shared/token/token.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Jukebox';
+
+  isSignedIn!: boolean;
+  constructor(
+    private auth: AuthStateService,
+    public router: Router,
+    public token: TokenService
+  ) {}
+
+  ngOnInit() {
+    this.auth.userAuthState.subscribe((val) => {
+      this.isSignedIn = val;
+    });
+  }
+  
+  // Signout
+  signOut() {
+    this.auth.setAuthState(false);
+    this.token.removeToken();
+    this.router.navigate(['login']);
+  }
 }
