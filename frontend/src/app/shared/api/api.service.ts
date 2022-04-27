9561df7ec8ded15b47237d85d8d17b2e0463fc9a
Stijndus <stijn@ardusit.nl>
2022-04-27 11:44:02 +0200

    Added the add playlist request

--- frontend/src/app/shared/api/api.service.ts
+++ frontend/src/app/shared/api/api.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private readonly URL = 'http://127.0.0.1:8000/api'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  addPlaylist(data: any){
+    return this.http.post(`${this.URL}/playlists`, data);
+  }
 }
