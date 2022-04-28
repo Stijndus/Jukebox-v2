d1a6dda3e84ac6a8d8fd2fe0730b9d70ccfc9181
Stijndus <stijn@ardusit.nl>
2022-04-28 09:47:06 +0200

    Added some calls

--- frontend/src/app/shared/api/api.service.ts
+++ frontend/src/app/shared/api/api.service.ts
@@ -9,7 +9,39 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
+  getPlaylistsFromUser(uid: number){
+    return this.http.get<any[]>(`${this.URL}/playlists/${uid}`);
+  }
+
+  getPlaylist(id: number){
+    return this.http.get<any[]>(`${this.URL}/playlists/${id}`);
+  }
+
   addPlaylist(data: any){
     return this.http.post(`${this.URL}/playlists`, data);
+  }
+
+  getSongs(){
+    return this.http.get<any[]>(`${this.URL}/songs`);
+  }
+
+  getSong(id: number){
+    return this.http.get<any[]>(`${this.URL}/songs/${id}`);
+  }
+
+  addSong(data: any){
+    return this.http.post(`${this.URL}/songs`, data);
+  }
+
+  getSongsFromPlaylist(id:number){
+    return this.http.get<any[]>(`${this.URL}/songs_playlist/${id}`);
+  }
+
+  deleteSongFromPlaylist(id:number){
+    return this.http.delete<any>(`${this.URL}/songs_playlist/${id}`);
+  }
+
+  addSongToPlaylsit(data:any){
+    return this.http.post(`${this.URL}/songs_playlist`, data);
   }
 }
